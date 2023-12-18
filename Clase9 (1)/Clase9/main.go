9a4d3b88215783838ad065fd9859e58daab31fac
AndresCalvo98 <luisandrescalvoarreaga@gmail.com>
2023-12-17 22:44:35 -0600

    Proyecto1
    faltan2 reportes

---
+++ Clase9 (1)/Clase9/main.go
@@ -0,0 +1,244 @@
+package main
+
+import (
+	"Clase9/estructuras/ArbolAVL"
+	"Clase9/estructuras/ColaPrioridad"
+	"Clase9/estructuras/Listas"
+	"Clase9/estructuras/MatrizDispersa"
+	"fmt"
+	"strconv"
+)
+
+var listaDobleCircular *Listas.ListaDobleCircular = &Listas.ListaDobleCircular{Inicio: nil, Longitud: 0}
+var listaDoble *Listas.ListaDoble = &Listas.ListaDoble{Inicio: nil, Longitud: 0}
+var colaPrioridad *ColaPrioridad.Cola = &ColaPrioridad.Cola{Primero: nil, Longitud: 0}
+var matrizDispersa *MatrizDispersa.Matriz = &MatrizDispersa.Matriz{Raiz: &MatrizDispersa.NodoMatriz{PosX: -1, PosY: -1, Dato: &MatrizDispersa.Dato{Carnet_Tutor: 0, Carnet_Estudiante: 0, Curso: "RAIZ"}}, Cantidad_Alumnos: 0, Cantidad_Tutores: 0}
+var arbolCursos *ArbolAVL.ArbolAVL = &ArbolAVL.ArbolAVL{Raiz: nil}
+
+var loggeado_estudiante string = ""
+
+func main() {
+	opcion := 0
+	salir := false
+
+	for !salir {
+		fmt.Print("\033[H\033[2J")
+		fmt.Println("1. Inicio de Sesion")
+		fmt.Println("2. Salir")
+		fmt.Scanln(&opcion)
+		switch opcion {
+		case 1:
+			MenuLogin()
+		case 2:
+			salir = true
+		}
+	}
+}
+
+func MenuLogin() {
+	fmt.Print("\033[H\033[2J")
+	usuario := ""
+	password := ""
+	fmt.Print("Usuario: ")
+	fmt.Scanln(&usuario)
+	fmt.Print("Password: ")
+	fmt.Scanln(&password)
+
+	if usuario == "a" && password == "a" {
+		fmt.Println("Administrador Inicio Sesion")
+		MenuAdmin()
+	} else if listaDoble.Buscar(usuario, password) {
+		loggeado_estudiante = usuario
+		MenuEstudiantes()
+	} else {
+		fmt.Println("ERROR EN CREDENCIALES!!!!")
+	}
+}
+
+func MenuAdmin() {
+	opcion := 0
+	salir := false
+	for !salir {
+		fmt.Print("\033[H\033[2J")
+		fmt.Println("1. Carga de Estudiantes Tutores")
+		fmt.Println("2. Carga de Estudiantes")
+		fmt.Println("3. Cargar de Cursos") //Falta
+		fmt.Println("4. Control de Estudiantes")
+		fmt.Println("5. Reportes") //Falta
+		fmt.Println("6. Salir")
+		fmt.Scanln(&opcion)
+		switch opcion {
+		case 1:
+			CargaTutores()
+			//colaPrioridad.LeerCSV("Tutores.csv")
+		case 2:
+			CargaEstudiantes()
+			//listaDoble.LeerCSV("Estudiante.csv")
+		case 3:
+			CargaCursos()
+			//arbolCursos.LeerJson("archivo.json")
+		case 4:
+			ControlEstudiantes()
+		case 5:
+			Reportes()
+		case 6:
+			salir = true
+		}
+
+	}
+}
+
+func Reportes() {
+	opcion := 0
+	salir := false
+	for !salir {
+		fmt.Print("\033[H\033[2J")
+		fmt.Println("1. Reporte de Alumnos")
+		fmt.Println("2. Reporte de Tutores aceptados")
+		fmt.Println("3. Reporte de asignaciones") 
+		fmt.Println("4. Reporte de Cursos")
+		fmt.Println("5. Salir")
+		fmt.Scanln(&opcion)
+		switch opcion {
+		case 1:
+			fmt.Println("Reporte 1")
+		case 2:
+			fmt.Println("Reporte 1")
+		case 3:
+			matrizDispersa.Reporte("Matriz.jpg")
+		case 4:
+			arbolCursos.Graficar()
+		case 5:
+			salir = true
+		}
+
+	}
+}
+
+func MenuEstudiantes() {
+	opcion := 0
+	salir := false
+	for !salir {
+		fmt.Println("1. Ver Tutores Disponibles")
+		fmt.Println("2. Asignarse Tutores")
+		fmt.Println("3. Salir")
+		fmt.Scanln(&opcion)
+		switch opcion {
+		case 1:
+			fmt.Print("\033[H\033[2J")
+			listaDobleCircular.Mostrar()
+		case 2:
+			AsignarCurso()
+		case 3:
+			salir = true
+		}
+	}
+}
+
+func AsignarCurso() {
+	opcion := ""
+	salir := false
+	for !salir {
+		fmt.Println("Teclee el codigo del curso: ")
+		fmt.Scanln(&opcion)
+		//Iria el primer If del Arbol (pendiente)
+		if arbolCursos.Busqueda(opcion) {
+			if listaDobleCircular.Buscar(opcion) {
+				TutorBuscado := listaDobleCircular.BuscarTutor(opcion)
+				estudiante, err := strconv.Atoi(loggeado_estudiante)
+				if err != nil {
+					break
+				}
+				matrizDispersa.Insertar_Elemento(estudiante, TutorBuscado.Tutor.Carnet, opcion)
+				matrizDispersa.Reporte("Matriz.jpg")
+				break
+			} else {
+				fmt.Println("No hay tutores para ese curso....")
+				break
+			}
+		} else {
+			fmt.Println("El curso no existe en el sistema")
+			break
+		}
+
+	}
+}
+
+func CargaTutores() {
+	fmt.Print("\033[H\033[2J")
+	ruta := ""
+	fmt.Print("Nombre de Archivo: ")
+	fmt.Scanln(&ruta)
+	colaPrioridad.LeerCSV(ruta)
+	fmt.Println("Se cargo a la Cola los tutores")
+}
+
+func CargaEstudiantes() {
+	fmt.Print("\033[H\033[2J")
+	ruta := ""
+	fmt.Print("Nombre de Archivo: ")
+	fmt.Scanln(&ruta)
+	listaDoble.LeerCSV(ruta)
+	fmt.Println("Se cargo los estudiantes")
+}
+
+func ControlEstudiantes() {
+    opcion := 0
+    salir := false
+
+    for !salir {
+        fmt.Print("\033[H\033[2J")
+        colaPrioridad.Primero_Cola()
+        fmt.Println("════════════════════")
+        fmt.Println("1. Aceptar")
+        fmt.Println("2. Rechazar")
+        fmt.Println("3. Salir")
+        fmt.Scanln(&opcion)
+        if opcion == 1 {
+            curso := colaPrioridad.Primero.Tutor.Curso
+            tutorCarnet := colaPrioridad.Primero.Tutor.Carnet
+            tutorNombre := colaPrioridad.Primero.Tutor.Nombre
+            tutorNota := colaPrioridad.Primero.Tutor.Nota
+
+            if tutorExistente := listaDobleCircular.BuscarTutor(curso); tutorExistente != nil {
+                if tutorExistente.Tutor.Nota < tutorNota {
+                    listaDobleCircular.SustituirTutor(tutorCarnet, tutorNombre, curso, tutorNota)
+                }
+            } else {
+                listaDobleCircular.Agregar(tutorCarnet, tutorNombre, curso, tutorNota)
+                colaPrioridad.Descolar()
+            }
+        } else if opcion == 2 {
+            colaPrioridad.Descolar()
+        } else if opcion == 3 {
+            salir = true
+        } else {
+            fmt.Println("Opcion invalida")
+        }
+    }
+}
+
+
+func CargaCursos() {
+	fmt.Print("\033[H\033[2J")
+	ruta := ""
+	fmt.Print("Nombre de Archivo: ")
+	fmt.Scanln(&ruta)
+	arbolCursos.LeerJson(ruta)
+	fmt.Println("Se cargaron los cursos")
+}
+
+
+//Comprobar Existencia de tutor
+			/*
+				if comprobar == true { //Significa que el curso ya tiene 1 tutor
+					if comprobarNotas == true { //Comprobar que alumno tiene mejor nota
+						SustituirTutor()
+					}
+				}else{
+					listaDobleCircular.Agregar(colaPrioridad.Primero.Tutor.Carnet, colaPrioridad.Primero.Tutor.Nombre, colaPrioridad.Primero.Tutor.Curso, colaPrioridad.Primero.Tutor.Nota)
+				}
+			*/
+
+			//listaDobleCircular.Agregar(colaPrioridad.Primero.Tutor.Carnet, colaPrioridad.Primero.Tutor.Nombre, colaPrioridad.Primero.Tutor.Curso, colaPrioridad.Primero.Tutor.Nota)
+			//colaPrioridad.Descolar()
